7271435eda716f0a5f17b0aace4aac8aa55340be
Raheen Fiza <raheen1895@gmail.com>
2026-04-01 16:07:44 +0500

    Refactor restaurant order system for clarity
    Refactored order handling and improved user prompts.

--- restaurant.py
+++ restaurant.py
@@ -1,29 +1,40 @@
-menu={
-    'Pizza':100,
-    'Pasta':120,
-    'Burger':70,
-    "Tea":60,
+menu = {
+    'Pizza': 100,
+    'Pasta': 120,
+    'Burger': 70,
+    'Tea': 60,
 }
-print("welcome to RF restuant")
-print(" pizza: Rs100\n pasta: Rs120\n burger:Rs70\n tea: Rs:60\n")
-item_1=input("what you want to order?").title()
-total_order=0
-if item_1 in menu:
-    total_order+=menu[item_1]
-    print(f"Your order {item_1} has been done")
-else:
-    print(f"this item {item_1} is not avalable")
-another_order=input("Do you want to add anything else (yes/no)?").title()
-if another_order=='Yes':
-    item_2=input("Enter your another order").title()
-    if item_2 in menu:
-        total_order+=menu[item_2]
-        print("your order has been done")
-    else:
-        print(f"ordered {item_2} is not available yet")
-else:
-    print(f"The total amount you have to pay is Rs{total_order}")
-print(f"total amount to pay Rs{total_order}")
 
+print("Welcome to RF Restaurant")
+print(" Pizza: Rs100\n Pasta: Rs120\n Burger: Rs70\n Tea: Rs60\n")
+
+total_order = 0
+order_placed = False
+
+item_1 = input("What do you want to order? ").title()
+
+if item_1 in menu:
+    total_order += menu[item_1]
+    order_placed = True
+    print(f"Your order '{item_1}' has been placed!")
+else:
+    print(f"Sorry, '{item_1}' is not available.")
+
+another_order = input("Do you want to add anything else? (yes/no) ").title()
+
+if another_order == 'Yes':
+    item_2 = input("Enter your next order: ").title()
+    if item_2 in menu:
+        total_order += menu[item_2]
+        order_placed = True
+        print(f"Your order '{item_2}' has been placed!")
+    else:
+        print(f"Sorry, '{item_2}' is not available.")
+
+# Only show total if at least one item was ordered
+if order_placed:
+    print(f"\nTotal amount to pay: Rs{total_order}")
+else:
+    print("No valid items were ordered.")
 
 
